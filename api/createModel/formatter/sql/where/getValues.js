8b6951f18dce3950cec10968879c2b468eb269c1
Martin Guardado <martinguardado@gmail.com>
2020-02-18 20:36:54 -0700

    fix bug to return $search value last in array

--- api/createModel/formatter/sql/where/getValues.js
+++ api/createModel/formatter/sql/where/getValues.js
@@ -1,9 +1,11 @@
-const keysFilter = require('./keysFilter');
-
 // Returns values for WHERE conditions as an array, including full-text search
 function getValues(query) {
-  const keys = Object.keys(query).filter(keysFilter);
-  const values = keys.map((key) => query[key]);
+  const values = Object.keys(query)
+    .filter((key) => !key.startsWith('$'))
+    .map((key) => query[key]);
+
+  if (query.$search) values.push(query.$search);
+
   return values;
 }
 
