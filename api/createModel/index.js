9b7d4fea96435478ebf3aa11979280859c2d707c
Martin Guardado <martinguardado@gmail.com>
2020-03-01 17:53:00 -0700

    fix: multiple models will use the same db pool by default

--- api/createModel/index.js
+++ api/createModel/index.js
@@ -5,6 +5,8 @@ const isType = require('../../util/isType');
 
 const createPool = require('../../db');
 
+let defaultPool;
+
 // Model methods return a promise via db.query(). The methods themselves don't need to be
 // async unless they use a query's results before returning them to the handler.
 
@@ -14,7 +16,14 @@ module.exports = function createModel(initConfig) {
   if (!collection || !isType('string', collection))
     throw Error('invalid model collection name');
 
-  const db = initConfig.db || createPool();
+  // The default pool is created the first time createPool is called
+  // without a custom 'db' parameter. Due to Node module caching, the
+  // default pool is reused, if needed, by future calls of createPool.
+  if (initConfig.db === undefined && defaultPool === undefined) {
+    defaultPool = createPool();
+  }
+
+  const db = initConfig.db || defaultPool;
 
   const config = { ...defaultConfig, ...initConfig };
   const table = identifier(collection);
