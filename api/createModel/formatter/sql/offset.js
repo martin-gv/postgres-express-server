87322d1fa16b3dcb8a7963de1ed037cfe868b9e8
Martin Guardado <martinguardado@gmail.com>
2020-03-03 21:03:26 -0700

    improve checking if option key is not passed in

--- api/createModel/formatter/sql/offset.js
+++ api/createModel/formatter/sql/offset.js
@@ -2,7 +2,7 @@ const isType = require('../../../../util/isType');
 
 module.exports = (config) =>
   function offsetSQL(query) {
-    if (!query.$offset) return `OFFSET ${config.defaultOffset}`;
+    if (query.$offset === undefined) return `OFFSET ${config.defaultOffset}`;
 
     if (!isType('number', query.$offset))
       throw Error(`invalid offset value on ${config.collection}`);
