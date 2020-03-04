cb880ea72ac26c455b9bc15c9185898dd42e37bb
Martin Guardado <martinguardado@gmail.com>
2020-03-03 20:56:46 -0700

    improve check for missing key

--- api/createModel/formatter/sql/limit.js
+++ api/createModel/formatter/sql/limit.js
@@ -2,7 +2,7 @@ const isType = require('../../../../util/isType');
 
 module.exports = (config) =>
   function limitSQL(query) {
-    if (!query.$limit) return `LIMIT ${config.defaultLimit}`;
+    if (query.$limit === undefined) return `LIMIT ${config.defaultLimit}`;
 
     if (!isType('number', query.$limit))
       throw Error(`invalid limit value on ${config.collection}`);
