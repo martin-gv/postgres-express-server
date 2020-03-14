61b0d70e521ccbc0ae7eb194dc1f7d894e5a1dcb
Martin Guardado <martinguardado@gmail.com>
2020-03-14 16:39:33 -0600

    fix glitch when using an empty string as search criteria

--- api/createModel/formatter/sql/where.js
+++ api/createModel/formatter/sql/where.js
@@ -7,7 +7,10 @@ const isType = require('../../../../util/isType');
 
 module.exports = (config) =>
   function whereSQL(query) {
-    const keys = Object.keys(query).filter(keysFilter);
+    const keys = Object.keys(query)
+      .filter(keysFilter) // filter out $keys except for $search
+      .filter((key) => query[key]); // check for non-blank values
+
     if (isEmpty(keys)) return ['', []];
 
     if (query.$search && !isType('string', query.$search))
